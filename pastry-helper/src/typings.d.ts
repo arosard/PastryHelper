552d7b333ce90f1560eaf9749c84673ea0d55749
Alexandre Rosard <alexandre.rosard@gmail.com>
2026-01-23 19:25:25 +0100

    fix: use app types instead of Any

--- pastry-helper/src/typings.d.ts
+++ pastry-helper/src/typings.d.ts
@@ -1,14 +1,17 @@
-export {};
+import type { FridgeItem } from './app/services/food-loader';
+import type { Category } from './app/services/category-loader';
 
 declare global {
   interface Window {
     foodApi: {
-      loadFridgeItems: () => Promise<any[]>;
-      saveFridgeItems: (items: any[]) => Promise<void>;
+      loadFridgeItems: () => Promise<FridgeItem[]>;
+      saveFridgeItems: (items: FridgeItem[]) => Promise<void>;
     };
     categoryApi: {
-        loadCategories: () => Promise<any[]>;
-        saveCategories: (items: any[]) => Promise<void>;
+        loadCategories: () => Promise<Category[]>;
+        saveCategories: (items: Category[]) => Promise<void>;
     };
   }
 }
+
+export {};
