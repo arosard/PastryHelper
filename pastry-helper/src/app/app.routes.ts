ba15d61e15e0ddc3f718cfbf3417462a0c4257be
Alexandre Rosard <alexandre.rosard@gmail.com>
2026-01-26 17:02:16 +0100

    feat: add navigation bars and router

--- pastry-helper/src/app/app.routes.ts
+++ pastry-helper/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'fridge', pathMatch: 'full' },
+    { path: 'fridge', loadComponent: () => import('./fridge/fridge').then(m => m.Fridge) },
+];
--- pastry-helper/src/app/app.ts
+++ pastry-helper/src/app/app.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { Fridge } from './fridge/fridge';
+import { Component, inject } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { AsyncPipe } from '@angular/common';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, Fridge],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+	selector: 'app-root',
+	imports: [
+		AsyncPipe,
+		RouterLink,
+		RouterOutlet,
+		MatButtonModule,
+		MatIconModule,
+		MatListModule,
+		MatSidenavModule,
+		MatToolbarModule,
+	],
+	templateUrl: './app.html',
+	styleUrl: './app.css'
 })
 export class App {
+	private breakpointObserver = inject(BreakpointObserver);
+
+	isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+		.pipe(
+			map(result => result.matches),
+			shareReplay()
+		);
 }
