cd8e53f51c6fd9fcd0e511889b991e725fc6f0b5
Alexandre Rosard <alexandre.rosard@gmail.com>
2026-01-26 17:05:24 +0100

    feat: add empty  home page/component

--- pastry-helper/src/app/app.routes.ts
+++ pastry-helper/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'fridge', pathMatch: 'full' },
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', pathMatch: 'full', loadComponent: () => import('./home/home').then(m => m.Home) },
     { path: 'fridge', loadComponent: () => import('./fridge/fridge').then(m => m.Fridge) },
 ];
